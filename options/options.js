e505764b11f6e92480f60d73acef419b5567c785
Crowdbotics <team@crowdbotics.com>
2023-02-24 18:25:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "golftech-39103",
+  url: "https://golftech-39103.botics.co",
+  api: "https://golftech-39103.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
